5458f526e3a98345163affaa58ad5eea2d1ed0e9
Paula Martínez <84521246+Paulonia14@users.noreply.github.com>
2024-08-21 18:13:55 -0300

    update 1

--- tp2-agentes-racionales/code/agent.py
+++ tp2-agentes-racionales/code/agent.py
@@ -1,26 +1,33 @@
 import random
 
 class Agent:
-    def __init__(self, env):
+    def __init__(self, env, lives=1000):
         self.env = env
+        self.lives = lives  #Vidas del agente (empieza con 1000)
 
     def up(self):
         self.env.accept_action('Up')
+        self.lives -= 1  #Cada acción resta una vida
 
     def down(self):
         self.env.accept_action('Down')
+        self.lives -= 1 
 
     def left(self):
         self.env.accept_action('Left')
+        self.lives -= 1 
 
     def right(self):
         self.env.accept_action('Right')
+        self.lives -= 1 
 
     def clean(self):
         self.env.accept_action('Clean')
+        self.lives -= 1 
 
     def idle(self):
         self.env.accept_action('Idle')
+        self.lives -= 1 
 
     def perspective(self):
         return self.env.is_dirty() #Si es que la pos está sucia
